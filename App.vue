79e8aed321a31f70cfbe95348fc33a547550fa7f
SusimRoy <76260471+SusimRoy@users.noreply.github.com>
2021-07-11 12:42:15 +0530

    Integrate pug

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template lang='pug'>
+div
+  h1 Apple2Spotify-Playlist
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #394f64;
+  margin-top: 60px;
+}
+</style>
