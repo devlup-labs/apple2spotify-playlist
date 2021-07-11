6275c7fcba948fdf25739e0a246d6f44bf903f0d
PanyalaSainathReddy <reddy.17@iitj.ac.in>
2021-07-11 20:54:33 +0530

    Update home.vue

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,16 +2,16 @@
 div(class=" md:flex justify-between gap-24")
   div(class="flex justify-center md:justify-start mx-auto")
     div(class=" mr-0 text-gray-300 text-3xl text-center md:text-left md:text-5xl")
-      h2(class=" mt-8") Convert an
-      h2(class=" mt-8") Apple playlist
-      h2(class=" mt-8") into a Spotify playlist.
+      h2.mt-8 Convert an
+      h2.mt-8 Apple playlist
+      h2.mt-8 into a Spotify playlist.
       button(@click="goto('stepper')" class=" mt-16 pt-2 pb-3 px-4 text-black hover:shadow-inner hover:text-black hover-effect text-center text-lg font-semibold rounded-full")
-        span(class="mx-4") Convert Playlist
+        span.mx-4 Convert Playlist
   div
     img(class="mt-6 mx-auto md:mt-0 md:h-96 md:mr-40"
       src="../../public/logo-trans1.gif"
       alt="logo-transformation")
-div(class="mt-80" ref="stepper")
+div.mt-80(ref="stepper")
   Stepper
 </template>
 
