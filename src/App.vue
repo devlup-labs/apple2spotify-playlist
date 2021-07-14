069cba4328652e0ce28362e5287f476f2a4dda60
SusimRoy <76260471+SusimRoy@users.noreply.github.com>
2021-07-14 22:42:43 +0530

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@ div
   h1(class="text-4xl m-5") Apple2Spotify-Playlist
   div
    input(class="text border py-2 px-3 text-grey-darkest rounded-xl m-5 border-blue-400" v-model="message" placeholder="Enter the apple playlist link here")
-   div(v-for="item in playlist_type")
-      label {{item.text}}
-      input(class="ml-2 h-4 w-4" type="checkbox" :value="item.text" v-model="private")
+   div
+      label Make your playlist private
+      input(class="ml-2 h-4 w-4" type="checkbox" v-model="private")
   button(class="rounded-lg border-blue-400 border-2 m-5 w-20") Convert
 </template>
 
@@ -18,9 +18,6 @@ export default {
   data(){
     return{
       message:'',
-      playlist_type:[
-        {id:0 , text: 'Make your playlist private'}
-      ],
       private : false
     }
   }
