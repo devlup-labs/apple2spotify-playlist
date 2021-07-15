401943303b29c7d45c0030c86bcae69d287d2912
RahulGopathi <gopathi.1@iitj.ac.in>
2021-07-15 09:39:35 +0530

    Resolve all conflicts

--- src/App.vue
+++ src/App.vue
@@ -50,6 +50,14 @@ export default {
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 nav {
   font-family: "Noto Sans JP", sans-serif;
 }
