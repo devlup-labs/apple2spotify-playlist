8658a2e82cdb3ca69396dc95f080a6e37c1cfedb
RahulGopathi <gopathi.1@iitj.ac.in>
2021-07-20 23:34:20 +0530

    Add fetch songs methods

--- src/components/stepper.vue
+++ src/components/stepper.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
 .stepper.pt-48
   h1.text.text-white.text-3xl.pb-10.mb-10.tracking-wide Spotify Login
-  p.text.text-white.tracking-wider.pt-10 Login To Spotify for Converting your Apple this.Playlist
+  p.text.text-white.tracking-wider.pt-10 Login To Spotify for Converting your Apple Playlist
   button(@click="loggingToSpotify" class="button transition duration-100 transform px-6 py-1 m-4 hover:scale-110 mt-20 pt-2 pb-3 text-black rounded-full bg-white ")
     span.tracking-widest.pr-7.pl-7.font-bold LOG IN
   div(class="field")
-   h1.text-white.text-3xl.mt-10.mb-2.tracking-wide this.Playlist link
+   h1.text-white.text-3xl.mt-10.mb-2.tracking-wide Playlist link
    input(class="text-gray-700 font-bold mb-2 rounded-full w-72 h-7" type="text" v-model="plink" placeholder="Enter the apple playlist link here")
   div(class="mt-5")
    label.text-white.tracking-wider.pt-10 Make your playlist private
