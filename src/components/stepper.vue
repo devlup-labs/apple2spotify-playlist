1a8ec96fabc7684c2b21d96ca0dc7b676fb80a5a
Ronak <ronakmohata@gmail.com>
2021-08-10 17:16:59 +0530

    replaced map with forEach

--- src/components/stepper.vue
+++ src/components/stepper.vue
@@ -11,19 +11,13 @@
   div.text-4xl.tracking-wider(v-bind:class = "(step === 2)?'text-white font-bold':(step < 2)?'text-gray-600':'text-green-500 font-semibold'") Step-2
   br
   div(class="box bg-gray-300 bg-opacity-10 rounded-2xl" v-if="step == 2")
-<<<<<<< HEAD
-    div(class="field")
-     h1.text-white.text-3xl.mt-10.mb-2.tracking-wide Playlist link
-     input(class="text-gray-700 font-bold mb-2 rounded-full w-72 h-7" type="text" v-model="pLink" placeholder="  Enter the apple playlist link here")
-=======
     div(class="btn")
      h1.text-white.text-3xl.my-2.tracking-wide.font-bold Playlist link
      input(class="text-gray-700 mb-2 rounded-sm px-4 h-7" type="text" v-model="pLink" placeholder="Enter the apple playlist link here")
->>>>>>> 0fa2cd5896859661e9e696000116d9567939295c
     div(class="mt-5")
      input(class="btn mr-2 h-4 w-4" type="checkbox" v-model="isprivate")
      label.text-white.tracking-wider.pt-8 Make your playlist private
-    button(class="button transition duration-100 transform px-6 py-1 m-4 hover:scale-110 mt-10 pt-2 pb-3 text-black rounded-full bg-white" @click="changeMessage")
+    button(class="button transition duration-100 transform px-6 py-1 m-4 hover:scale-110 mt-10 pt-2 pb-3 text-black rounded-full bg-white" @click="getPlaylistInfoFromApple(this.pLink)")
      span.tracking-widest.pr-7.pl-7.font-bold CONVERT
   div(v-if="this.started")
     loader(:render="this.started" :text="this.message")
@@ -39,13 +33,9 @@
 import axios from "axios";
 import loader from "./loader";
 export default {
-<<<<<<< HEAD
   props:[
-    'spotifyToken'
+    'spotifyToken','step'
   ],
-=======
-  props: ['step'],
->>>>>>> 0fa2cd5896859661e9e696000116d9567939295c
   components: { loader },
   data() {
     return {
@@ -153,7 +143,7 @@ export default {
     searchingSongsInSpotify() {
       this.songsUri.length=0
       this.songsNotFound.length=0
-      this.playlist["songs"].map((song) => {
+      this.playlist["songs"].forEach((song) => {
         axios
           .get("https://api.spotify.com/v1/search", {
             headers: {
@@ -168,7 +158,7 @@ export default {
             let items = res.data.tracks.items;
             let i = 0
             while( items.length>i) {
-              if (items[i].artists[0].name.toUpperCase() === song.artist.toUpperCase() && items[i].name.toUpperCase() === song.songname.toUpperCase()) {
+              if (items[i].artists[0].name.toUpperCase() == song.artist.toUpperCase() && items[i].name.toUpperCase() == song.songname.toUpperCase()) {
                 this.songsUri.push(items[i].uri);
                 break;
               }
